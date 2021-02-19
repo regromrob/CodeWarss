42d9cfe08ae99c34ff257185590ed879ff0736e4
regromrob <romae31@gmail.com>
2021-02-18 21:08:19 -0500

    cw

--- superDuperEasy/main.js
+++
@@ -1,3 +0,0 @@
-// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
-
-const problem = x => typeof x ==='number'? x * 50 +6: "Error"
