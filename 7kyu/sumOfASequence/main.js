1c10865449995db9645943402779c929acb2d015
regromrob <romae31@gmail.com>
2021-07-04 22:14:20 -0400

    sum

---
+++ 7kyu/sumOfASequence/main.js
@@ -0,0 +1,30 @@
+// Your task is to make function, which returns the sum of a sequence of integers.
+
+// The sequence is defined by 3 non-negative values: begin, end, step.
+
+// If begin value is greater than the end, function should returns 0
+
+// Examples
+
+// sequenceSum(2,2,2) === 2
+// sequenceSum(2,6,2) === 12 // 2 + 4 + 6
+// sequenceSum(1,5,1) === 15 // 1 + 2 + 3 + 4 + 5
+// sequenceSum(1,5,3) === 5 // 1 + 4
+// This is the first kata in the series:
+
+// Sum of a sequence (this kata)
+// Sum of a Sequence [Hard-Core Version]
+
+function sequenceSum (begin, end, step) {
+    if(begin > end){
+      return 0
+    }else{
+      let sum=begin
+       return sequenceSum(begin + step, end,step) + begin
+    }
+  };
+  
+  // sequenceSum(2,6,2) === 12 
+  //2+2,6 (4,6,2)
+  //4 +2,6 (6,6,2)
+  //2+2,6,2 (4,6,2) , 4+2,6,2(6,6,2)
