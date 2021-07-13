9f9b340712c28f1392f36e156626920a111511d7
regromrob <romae31@gmail.com>
2021-07-12 22:12:31 -0400

    string

---
+++ 7kyu/findMinandMax/main.js
@@ -0,0 +1,9 @@
+//Implement a function that returns the minimal and the maximal value of a list (in this order).
+
+
+
+function getMinMax(arr){
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
+  return [min,max]
+  };
